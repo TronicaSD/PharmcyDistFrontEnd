272e26906f20e9cb933d5b512a86bd1a7dd940df
alaa saleh <alaaasaleh7@gmail.com>
2021-01-04 17:46:11 +0200

    s

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,16 +8,14 @@ import { DrugsComponent } from './components/drugs/drugs.component'
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from '../auth/services/auth.service';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PharmcyComponent } from './components/pharmcy/pharmcy.component';
 import { StockDetailsComponent } from './components/stock-details/stock-details.component';
 import { SamplesComponent } from './components/samples/samples.component';
 import { InvoiceComponent } from './components/invoice/invoice.component';
 
 import { PublicService } from '../core/publicService.Service';
-import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { AuthGuard } from '../auth/services/auth.guard';
-import { NbSidebarService, NbThemeModule } from '@nebular/theme';
+import {  NbThemeModule } from '@nebular/theme';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
